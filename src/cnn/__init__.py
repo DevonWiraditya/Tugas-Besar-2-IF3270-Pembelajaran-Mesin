4bb315bcd603e25f250a0c18da483ed7fe2c9bcd
FPSneeder <19623039@std.stei.itb.ac.id>
2026-05-11 21:09:17 +0700

    feat: add CNN dataset utilities

--- src/cnn/__init__.py
+++ src/cnn/__init__.py
@@ -1,3 +1,10 @@
 from src.cnn.config import CNNConfig, load_cnn_config
+from src.cnn.data import CNNDataset, ImageRecord, load_cnn_dataset
 
-__all__ = ["CNNConfig", "load_cnn_config"]
+__all__ = [
+    "CNNConfig",
+    "CNNDataset",
+    "ImageRecord",
+    "load_cnn_config",
+    "load_cnn_dataset",
+]
